fb5cfad23306af38a94d1fe1f9f48b5f056d4f19
William Morland <wmorland@fb.com>
2024-12-05 13:17:46 +0000

    My solution for Part 2

--- 2024/Day1/day1.py
+++ 2024/Day1/day1.py
@@ -7,3 +7,15 @@ col1, col2 = np.loadtxt("input.txt", dtype=int, unpack=True)
 s = np.sum(np.abs(np.sort(col1) - np.sort(col2)))
 
 print(s)
+
+# Part 2
+values, counts = np.unique(col2, return_counts=True)
+
+freq = dict(zip(values, counts))
+
+similarity = 0
+for i in range(len(col1)):
+    if (col1[i] in freq):
+        similarity += col1[i] * freq[col1[i]]
+
+print(similarity)
