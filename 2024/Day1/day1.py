b51e86af9d0360c9f95600ee75634c57506edef2
William Morland <wmorland@fb.com>
2024-12-05 13:07:44 +0000

    Improvements from ChatGPT

--- 2024/Day1/day1.py
+++ 2024/Day1/day1.py
@@ -1,15 +1,9 @@
 import numpy as np
 
-data = np.loadtxt("input.txt", dtype=int, unpack=True)
+# Load the data and sort the columns directly
+col1, col2 = np.loadtxt("input.txt", dtype=int, unpack=True)
 
-col1 = data[0]
-col2 = data[1]
-
-sort1 = np.sort(col1)
-sort2 = np.sort(col2)
-
-s = 0
-for i in range(len(sort1)):
-    s += abs(sort1[i]-sort2[i])
+# Sort both columns and compute the sum of absolute differences
+s = np.sum(np.abs(np.sort(col1) - np.sort(col2)))
 
 print(s)
