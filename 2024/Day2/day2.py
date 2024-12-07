b499fb09c7fec1b63672a2f0e5e2ebf9ae128287
William Morland <wmorland@fb.com>
2024-12-07 12:27:10 +0000

    Solved Day 2 Part 2

--- 2024/Day2/day2.py
+++ 2024/Day2/day2.py
@@ -20,3 +20,26 @@ def is_safe(arr):
 
 safe_arrs = [is_safe(x) for x in input]
 print(np.sum(safe_arrs))
+
+def damper_is_safe(arr):
+    if arr[0] < arr[1]:
+        min = 1
+        max = 3
+    else:
+        min = -3
+        max = -1
+    for i in range(1, len(arr)):
+        d = arr[i] - arr[i-1]
+        if d > max or d < min:
+            before = arr[:i-1] + arr[i:]
+            after = arr[:i] + arr[i+1:]
+            rfirst = arr[1:]
+            rsecond = arr[:1] + arr[2:]
+            if (is_safe(before) == 1) or (is_safe(after) == 1) or (is_safe(rfirst) == 1) or (is_safe(rsecond) == 1):
+                return 1
+            else:
+                return 0
+    return 1
+
+damper_arrs = [damper_is_safe(x) for x in input]
+print(np.sum(damper_arrs))
