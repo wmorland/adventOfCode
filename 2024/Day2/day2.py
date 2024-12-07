165fa7bc60fd7cfb806eb36dbac8657bc94c2d4b
William Morland <wmorland@fb.com>
2024-12-07 12:29:41 +0000

    Add black python formatter

--- 2024/Day2/day2.py
+++ 2024/Day2/day2.py
@@ -5,6 +5,7 @@ with open("input.txt") as f:
 
 input = [[int(n) for n in l.split(" ")] for l in tl]
 
+
 def is_safe(arr):
     if arr[0] < arr[1]:
         min = 1
@@ -13,13 +14,15 @@ def is_safe(arr):
         min = -3
         max = -1
     for i in range(1, len(arr)):
-        d = arr[i] - arr[i-1]
+        d = arr[i] - arr[i - 1]
         if d > max or d < min:
             return 0
     return 1
 
+
 safe_arrs = [is_safe(x) for x in input]
 print(np.sum(safe_arrs))
+
 
 def damper_is_safe(arr):
     if arr[0] < arr[1]:
@@ -29,17 +32,23 @@ def damper_is_safe(arr):
         min = -3
         max = -1
     for i in range(1, len(arr)):
-        d = arr[i] - arr[i-1]
+        d = arr[i] - arr[i - 1]
         if d > max or d < min:
-            before = arr[:i-1] + arr[i:]
-            after = arr[:i] + arr[i+1:]
+            before = arr[: i - 1] + arr[i:]
+            after = arr[:i] + arr[i + 1 :]
             rfirst = arr[1:]
             rsecond = arr[:1] + arr[2:]
-            if (is_safe(before) == 1) or (is_safe(after) == 1) or (is_safe(rfirst) == 1) or (is_safe(rsecond) == 1):
+            if (
+                (is_safe(before) == 1)
+                or (is_safe(after) == 1)
+                or (is_safe(rfirst) == 1)
+                or (is_safe(rsecond) == 1)
+            ):
                 return 1
             else:
                 return 0
     return 1
+
 
 damper_arrs = [damper_is_safe(x) for x in input]
 print(np.sum(damper_arrs))
