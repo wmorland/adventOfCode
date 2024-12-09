efd23e1d346aa716917c0392ef82236886ed9a04
William Morland <wmorland@fb.com>
2024-12-09 11:41:40 +0000

    Day 3 Part 2 initial working solution

--- 2024/Day3/day3.py
+++ 2024/Day3/day3.py
@@ -1,12 +1,24 @@
 import re
 
-total = 0
+
+def calc_mul_for_string(total, l):
+    matches = re.findall(r"(?:mul\()(\d+,\d+)(?:\))", l)
+    for match in matches:
+        a, b = match.split(",", 1)
+        total += int(a) * int(b)
+    return total
+
 
 with open("input.txt") as f:
-    for l in f.readlines():
-        matches = re.findall(r"(?:mul\()(\d+,\d+)(?:\))", l)
-        for match in matches:
-            a, b = match.split(",", 1)
-            total += int(a) * int(b)
+    l = f.read()
 
+total = 0
+total = calc_mul_for_string(total, l)
+print(total)
+
+total = 0
+enablers = l.split("do()")
+for enabled in enablers:
+    calc = enabled.split("don't()", 1)[0]
+    total = calc_mul_for_string(total, calc)
 print(total)
